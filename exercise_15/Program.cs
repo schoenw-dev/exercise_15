a68fb1ce10a189ca5bcdeae0c96bcef1b4c67828
will <wschoenle93@gmail.com>
2021-04-14 13:50:21 -0400

    first commit for exercise 15

---
+++ exercise_15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("Enter a number:");
+                string userInput = Console.ReadLine();
+                int userNumber = int.Parse(userInput);
+
+
+
+                for (int i = 1; i <= userNumber; i++)
+                {
+
+                    Console.WriteLine(i * i * i);
+
+                }
+
+                Console.WriteLine("Would you like to continue? (y|n)");
+                string userAnswer = Console.ReadLine();
+
+                if (userAnswer == "y")
+                {
+                    repeat = true;
+                }
+                else if (userAnswer == "n")
+                {
+                    repeat = false;
+                }
+
+
+            }
+
+            Console.WriteLine("Goodbye!");
+
+        }
+    }
+}
